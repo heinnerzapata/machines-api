4e7ca7fc01edd839fc4d852d5f9045f4b4e0cc34
Heinner Zapata <heinnerzapata1991@gmail.com>
2023-09-26 08:32:10 -0500

    using docker

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,5 @@
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 module.exports = {
-  //url: `mongodb+srv://heinnerzapata1991:gabdOOxEDcrC3Zvd@machines01.ubpmwpq.mongodb.net/?retryWrites=true&w=majority`,
   url: `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}?retryWrites=true&w=majority`,
 };
